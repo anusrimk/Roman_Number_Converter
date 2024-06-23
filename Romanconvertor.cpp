284f89700e7fcfcd381c42b85162743166e76a26
Anusri Karmokar <142775407+anusrimk@users.noreply.github.com>
2024-06-23 14:11:51 +0530

    Add files via upload

---
+++ Romanconvertor.cpp
@@ -0,0 +1,226 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+
+int main(){ 
+    string n , roman;
+    int num = 0;
+
+    cout<<"Enter the roman number (<4000) : \n";
+    cin>>n;
+    
+    for(int i = 0 ; i<n.length() ; i++){
+        roman += toupper(n[i]);
+    }
+    
+    switch(n.length())
+    {
+        case 1:
+            if(roman == "I")
+            {
+                num += 1;
+                break;
+            }
+            if(roman == "V")
+            {
+                num += 5;
+                break;
+            }
+            if(roman == "L")
+            {
+                num += 50;
+                break;
+            }
+            if(roman == "X")
+            {
+                num += 10;
+                break;
+            }
+            if(roman == "C")
+            {
+                num += 100;
+                break;
+            }
+            if(roman == "M")
+            {
+                num += 1000;
+                break;
+            }
+            if(roman == "D")
+            {
+                num += 500;
+                break;
+            }
+            break;
+
+        default:
+            for(int i = 0 ; i<n.length() ; i++)
+            {
+                if(roman[i] == 'I')
+                {
+                    if(roman[i+1] == 'V' || roman[i+1] == 'X' || roman[i+1] == 'L' || roman[i+1] == 'C' || roman[i+1] == 'D' || roman[i+1] == 'M')
+                    {
+                        num+=0;
+                    }
+                    else
+                    {
+                        num+=1;
+                    }
+                }
+
+                if(roman[i] == 'V')
+                {
+                    if( roman[i+1] == 'X' || roman[i+1] == 'L' || roman[i+1] == 'C' || roman[i+1] == 'D' || roman[i+1] == 'M')
+                    {
+                        num+=0;
+                    }
+                    else
+                    {
+                        if(roman[i-1] == 'I')
+                        {
+                            num-=1;
+                        }
+
+                        num += 5;
+                    }
+                }
+
+                if(roman[i] == 'X')
+                {
+                    if( roman[i+1] == 'L' || roman[i+1] == 'C' || roman[i+1] == 'D' || roman[i+1] == 'M')
+                    {
+                        num+=0;
+                    }
+                    else
+                    {
+                        if(roman[i-1] == 'I')
+                        {
+                            num-=1;
+                        }
+                        if(roman[i-1] == 'V')
+                        {
+                            num -= 5;
+                        }
+                        
+                        num += 10;
+                        
+
+                    }
+                    
+                }
+
+                if(roman[i] == 'L')
+                {
+                    if(roman[i+1]=='C' || roman[i+1] == 'D' || roman[i+1] == 'M'){
+                        num+=0;
+                    }
+                    else
+                    {
+                        if(roman[i-1] == 'X')
+                        {
+                            num -= 10;
+                        }
+
+                        if(roman[i-1] == 'V')
+                        {
+                            num -= 5;
+                        }
+                        if(roman[i-1] == 'I')
+                        {
+                            num -= 1;
+                        }
+                            num+=50;
+                    }
+
+                }
+
+                if(roman[i] == 'C')
+                {
+                    if( roman[i+1] == 'D' || roman[i+1] == 'M'){
+                        num+=0;
+                    }
+                    else
+                    {
+                        if(roman[i-1] == 'L')
+                        {
+                            num -= 50;
+                        }
+                        if(roman[i-1] == 'X')
+                        {
+                            num-=10;
+                        }
+                        if(roman[i-1] == 'V')
+                        {
+                            num -= 5;
+                        }
+                        if(roman[i-1] == 'I')
+                        {
+                            num -= 1;
+                        }
+
+                        num += 100; 
+                    }   
+                }
+
+                
+
+                if(roman[i] == 'D')
+                {
+                    if(roman[i+1] == 'M'){
+                        num+=0;
+                    }
+                    else{
+                        if(roman[i-1] == 'I'){
+                            num -=1;
+                        }
+                        if(roman[i-1]=='V'){
+                            num-=5;
+                        }
+                        if(roman[i-1] == 'X'){
+                            num -= 10;
+                        }
+                        if(roman[i-1] == 'L'){
+                            num -= 50;
+                        }
+                        if(roman[i-1] == 'C'){
+                            num -= 100;
+                        }
+                        num+=500;
+                    }
+                    
+                }
+
+                
+
+                
+
+                if(roman[i] == 'M')
+                {
+                        if(roman[i-1] == 'I'){
+                            num -=1;
+                        }
+                        if(roman[i-1]=='V'){
+                            num-=5;
+                        }
+                        if(roman[i-1] == 'X'){
+                            num -= 10;
+                        }
+                        if(roman[i-1] == 'L'){
+                            num -= 50;
+                        }
+                        if(roman[i-1] == 'C'){
+                            num -= 100;
+                        }    
+                        if(roman[i-1] == 'D'){
+                            num -= 500;
+                        }
+                    num+= 1000;
+                }
+            }
+    }
+
+    cout<<num;
+    return 0;
+}
